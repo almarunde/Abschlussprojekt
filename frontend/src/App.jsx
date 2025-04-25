5eb91978f1942f100480ffdec7aa7cea621a8523
almarunde <almarunde3@gmail.com>
2025-04-25 15:35:23 +0200

    Erste React-Versuche

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Header from "./Header.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-
+          <Header/>
       </div>
       <h1>Welcome page</h1>
       <div className="card">
---
+++ frontend/src/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+import App from "./App.jsx";
+
+function Header() {
+    return(
+        <>
+            <div id={"header"}>
+                <p className={"fonts"}>dhifef</p>
+            </div>
+        </>
+    )
+
+}
+
+export default Header

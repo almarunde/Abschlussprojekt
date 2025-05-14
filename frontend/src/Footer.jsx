58ee14b3ff46a1c0974158a9df093f6d27036d5a
almarunde <almarunde3@gmail.com>
2025-05-14 11:21:10 +0200

    FE - Design Unclean

---
+++ frontend/src/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <>
+            <div id={"footer"}>
+                <p>&copy; Alma Runde - Abteilung Service und Support - Team Clientintegration</p>
+            </div>
+        </>
+    )
+}
+
+
+export default Footer
--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,11 +1,17 @@
 import './Header.css'
-import App from "./App.jsx";
+import Logo_Charite from "./assets/Logo_Charite.png"
+import Pakulfus from "./assets/PaketierungsWizard.png"
 
 function Header() {
-    return(
+    return (
         <>
             <div id={"header"}>
-                <p className={"fonts"}>dhifef</p>
+                <div id={"linkeSpalte"} className={"spalten"}><img src={Logo_Charite} id={"logo"}></img></div>
+                <div id={"mittlereSpalte"} className={"spalten"}></div>
+                <div id={"rechteSpalte"} className={"spalten"}><img src={Pakulfus} id={"pakulfus"}/>
+                    <p id={"titelTool"}>Paketierungstool</p>
+                    <p id={"untertitelTool"}>Abteilung Service und Support - Team Clientintegration</p>
+                </div>
             </div>
         </>
     )
